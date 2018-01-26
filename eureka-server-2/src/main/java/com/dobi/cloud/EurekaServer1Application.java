b8fcfaaca0d233257b09fbec5c6e78017f311e09
xujiping <xjpdyx@hotmail.com>
2018-01-26 15:04:15 +0800

    增加eureka高可用服务中心

---
+++ eureka-server-2/src/main/java/com/dobi/cloud/EurekaServer1Application.java
@@ -0,0 +1,18 @@
+package com.dobi.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * eureka服务发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer1Application.class, args);
+    }
+}
